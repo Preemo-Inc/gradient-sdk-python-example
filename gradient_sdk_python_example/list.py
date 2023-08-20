cdeadb22b9d9cd2b5faed459ed13af27fc9129d5
Adrian Miguel <adriantmiguel@gmail.com>
2023-08-19 18:06:13 -0700

    saving changes

--- gradient_sdk_python_example/list.py
+++ gradient_sdk_python_example/list.py
@@ -10,6 +10,9 @@ configuration = gradientai.Configuration(
 )
 
 models_api = gradientai.ModelsApi(gradientai.ApiClient(configuration))
-model_list = models_api.list_models(only_base="false", x_preemo_workspace_id=os.getenv("GRADIENT_WORKSPACE_ID"))
+model_list = models_api.list_models(
+    only_base=False,
+    x_preemo_workspace_id=os.getenv("GRADIENT_WORKSPACE_ID")
+)
 
 print(model_list.models[0].actual_instance)
