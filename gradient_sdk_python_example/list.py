0b2dbcfaf9e5666d485dbbfca59e79a401836aca
Adrian Miguel <adriantmiguel@gmail.com>
2023-08-21 09:51:28 -0700

    adding with

--- gradient_sdk_python_example/list.py
+++ gradient_sdk_python_example/list.py
@@ -8,9 +8,10 @@ configuration = gradientai.Configuration(
     access_token=os.getenv("GRADIENT_ACCESS_TOKEN")
 )
 
-models_api = gradientai.ModelsApi(gradientai.ApiClient(configuration))
-model_list = models_api.list_models(
-    x_gradient_workspace_id=os.getenv("GRADIENT_WORKSPACE_ID")
-)
+with gradientai.ApiClient(configuration) as api_client:
+    models_api = gradientai.ModelsApi(api_client)
+    model_list = models_api.list_models(
+        x_gradient_workspace_id=os.getenv("GRADIENT_WORKSPACE_ID")
+    )
 
 print(model_list.models[0].actual_instance)
