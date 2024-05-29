02fec7e1d796a0552b8b6a230ee550995c174c7f
GradientBot <it-admin@gradient.ai>
2024-05-29 14:15:27 -0700

    Project import generated by Copybara.
    GitOrigin-RevId: 7aca87c053f49b96ae8cf15d57490a040582111d

--- gradient_sdk_python_example/rag.py
+++ gradient_sdk_python_example/rag.py
@@ -1,27 +1,54 @@
 from dotenv import load_dotenv
-from gradientai import Gradient, SimpleNodeParser
+from gradientai import Gradient, SentenceWindowNodeParser, SimpleNodeParser
 
 load_dotenv()
 
 
-def main():
+def main() -> None:
     gradient = Gradient()
 
-    rag_collection = gradient.create_rag_collection(
+    sentence_window_node_parser_rag_collection = gradient.create_rag_collection(
         filepaths=[
             "resources/Lorem_Ipsum.pdf",
         ],
-        name="My RAG collection",
+        name="RAG Collection with Sentence Window Node Parser",
+        parser=SentenceWindowNodeParser(
+            chunk_size=1024, chunk_overlap=20, window_size=3
+        ),
+        slug="bge-large",
+    )
+    print(
+        f"Created RAG collection with id: {sentence_window_node_parser_rag_collection.id_}"
+    )
+    print(f"RAG collection: {sentence_window_node_parser_rag_collection}")
+
+    sentence_window_node_parser_rag_collection.add_files(
+        filepaths=["resources/Life_Kit.mp3"]
+    )
+    print(
+        f"RAG collection files: {sentence_window_node_parser_rag_collection.files}"
+    )
+
+    simple_node_parser_rag_collection = gradient.create_rag_collection(
+        filepaths=[
+            "resources/Lorem_Ipsum.pdf",
+        ],
+        name="RAG Collection with Simple Node Parser",
         parser=SimpleNodeParser(chunk_size=1024, chunk_overlap=20),
         slug="bge-large",
     )
-    print(f"Created RAG collection with id: {rag_collection.id_}")
-    print(f"RAG collection: {rag_collection}")
+    print(
+        f"Created RAG collection with id: {simple_node_parser_rag_collection.id_}"
+    )
+    print(f"RAG collection: {simple_node_parser_rag_collection}")
 
-    rag_collection.add_files(filepaths=["resources/Life_Kit.mp3"])
-    print(f"RAG collection files: {rag_collection.files}")
+    simple_node_parser_rag_collection.add_files(
+        filepaths=["resources/Life_Kit.mp3"]
+    )
+    print(f"RAG collection files: {simple_node_parser_rag_collection.files}")
 
-    rag_collection.delete()
+    sentence_window_node_parser_rag_collection.delete()
+    simple_node_parser_rag_collection.delete()
 
     gradient.close()
 
