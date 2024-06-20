ace3a5a7995fdbb63097ec9d6a10af632a4ea091
GradientBot <it-admin@gradient.ai>
2024-06-20 14:39:01 -0700

    Project import generated by Copybara.
    GitOrigin-RevId: 30eb3038e4c40e8cb8a2f8fad7f22f9018f39eb0

--- gradient_sdk_python_example/rag.py
+++ gradient_sdk_python_example/rag.py
@@ -1,54 +1,78 @@
 from dotenv import load_dotenv
-from gradientai import Gradient, SentenceWindowNodeParser, SimpleNodeParser
+from gradientai import (
+    FileChunker,
+    Gradient,
+    MeaningBasedChunker,
+    NormalChunker,
+    RAGChunker,
+    RAGCollection,
+    SentenceWithContextChunker,
+)
 
 load_dotenv()
+
+FILE_CHUNKER = FileChunker()
+
+MEANING_BASED_CHUNKER_CONFIG = MeaningBasedChunker(
+    overlap=20,
+    sentence_group_length=1,
+    similiarity_percent_threshold=95,
+    size=1024,
+)
+
+NORMAL_CHUNKER_CONFIG = NormalChunker(
+    overlap=20,
+    size=1024,
+)
+
+SENTECE_WITH_CONTEXT_CHUNKER_CONFIG = SentenceWithContextChunker(
+    context_sentences=3,
+    overlap=20,
+    size=1024,
+)
+
+
+def build_rag_collection(
+    gradient: Gradient, chunker: RAGChunker
+) -> RAGCollection:
+    rag_collection = gradient.create_rag_collection(
+        filepaths=[
+            "resources/Lorem_Ipsum.pdf",
+        ],
+        name=f"RAG Collection with {chunker.chunker_type}",
+        chunker=chunker,
+        slug="bge-large",
+    )
+    print(f"Created RAG collection with id: {rag_collection.id_}")
+    print(f"Chunker: {rag_collection.chunker}")
+
+    rag_collection.add_files(filepaths=["resources/Life_Kit.mp3"])
+    print(f"RAG collection files: {rag_collection.files}")
+
+    return rag_collection
 
 
 def main() -> None:
     gradient = Gradient()
 
-    sentence_window_node_parser_rag_collection = gradient.create_rag_collection(
-        filepaths=[
-            "resources/Lorem_Ipsum.pdf",
-        ],
-        name="RAG Collection with Sentence Window Node Parser",
-        parser=SentenceWindowNodeParser(
-            chunk_size=1024, chunk_overlap=20, window_size=3
-        ),
-        slug="bge-large",
-    )
-    print(
-        f"Created RAG collection with id: {sentence_window_node_parser_rag_collection.id_}"
-    )
-    print(f"RAG collection: {sentence_window_node_parser_rag_collection}")
+    file_chunker_rag_collection = build_rag_collection(gradient, FILE_CHUNKER)
 
-    sentence_window_node_parser_rag_collection.add_files(
-        filepaths=["resources/Life_Kit.mp3"]
-    )
-    print(
-        f"RAG collection files: {sentence_window_node_parser_rag_collection.files}"
+    meaning_based_chunker_rag_collection = build_rag_collection(
+        gradient, MEANING_BASED_CHUNKER_CONFIG
     )
 
-    simple_node_parser_rag_collection = gradient.create_rag_collection(
-        filepaths=[
-            "resources/Lorem_Ipsum.pdf",
-        ],
-        name="RAG Collection with Simple Node Parser",
-        parser=SimpleNodeParser(chunk_size=1024, chunk_overlap=20),
-        slug="bge-large",
+    normal_chunker_rag_collection = build_rag_collection(
+        gradient, NORMAL_CHUNKER_CONFIG
     )
-    print(
-        f"Created RAG collection with id: {simple_node_parser_rag_collection.id_}"
-    )
-    print(f"RAG collection: {simple_node_parser_rag_collection}")
 
-    simple_node_parser_rag_collection.add_files(
-        filepaths=["resources/Life_Kit.mp3"]
+    sentence_with_context_chunker_rag_collection = build_rag_collection(
+        gradient, SENTECE_WITH_CONTEXT_CHUNKER_CONFIG
     )
-    print(f"RAG collection files: {simple_node_parser_rag_collection.files}")
 
-    sentence_window_node_parser_rag_collection.delete()
-    simple_node_parser_rag_collection.delete()
+    file_chunker_rag_collection.delete()
+    meaning_based_chunker_rag_collection.delete()
+    normal_chunker_rag_collection.delete()
+    sentence_with_context_chunker_rag_collection.delete()
 
     gradient.close()
 
